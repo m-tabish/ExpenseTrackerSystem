0c8e0bb3f5e0cf0f86c19bb94e1ce575b5649be2
Tabish <mohdtabishkhan001@gmail.com>
2023-10-07 17:12:02 +0530

    learnt till radioButtons

--- ETS_Files/main.py
+++ ETS_Files/main.py
@@ -1,1 +1,20 @@
-print("Learning Tkinter")
+from tkinter import *
+root = Tk()
+root.title("Tabish")
+root.geometry("1440x1000")
+
+
+# main_frame = LabelFrame(root,text= "Currency Converter", padx= 50, pady=50)
+title = Label(root, text = "Currency Converter", justify= CENTER,)
+# amount = Label(root, text = "Amount")
+# amt_input = Entry(root, width="100")
+# btn_convert = Button(root, text = "Convert")
+
+# main_frame.pack(padx=10, pady=10)
+title.pack()
+# amount.grid(row=1 , column = 0)
+# amt_input.grid(row=1 , column = 1)
+# btn_convert.grid(row=1 , column = 2)
+
+
+root.mainloop()
---
+++ ETS_Files/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+amt_input.grid(row=1 , column = 1)
+btn_convert.grid(row=1 , column = 2)
